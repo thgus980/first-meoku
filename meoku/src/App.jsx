32531626f9a4a435aa78bfed222bade86372b7b9
penguin980 <thgus980@naver.com>
2024-02-12 18:39:41 +0900

    first commit

--- meoku/src/App.jsx
+++ meoku/src/App.jsx
@@ -1,11 +1,28 @@
 import './App.css'
+import React from 'react';
+import logo_font from './images/logo_font.png'
+import logo_img from './images/logo_img.png'
 
 function App() {
 
     return (
-        <>
-            test comment
-        </>
+        <div className="App">
+            <div className="nav">
+                <div className='logo'>
+                    <img src={logo_img} width="36px" height="41px" />
+                    <img src={logo_font} width="202px" height="28px" />
+                </div>
+                <div className='menu'>
+                    <p>구내식당표</p>
+                </div>
+                <div className='menu'>
+                    <p>맛집리스트</p>
+                </div>
+                <div className='menu'>
+                    <p>AI 추천</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
